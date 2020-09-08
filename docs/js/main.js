f1bafb32b5684e50c42edb6765fcd5826f2cfa31
Vladimir <win_1994@mail.ru>
2020-09-08 17:26:35 +0300

    mobMenu

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,44 @@
+// Taggle menu
+$(document).ready(function(){
+
+    const menuToggle = document.querySelector('.toggle-menu');
+    const mobMenu = document.querySelector('.nav-list');
+    const overlaeyEl = document.querySelector('#overlay');
+    const bodyEl = document.body;
+
+    // Клик по иконке гамбургер
+    menuToggle.addEventListener('click', function(){
+        this.classList.toggle('active');
+        mobMenu.classList.toggle('active');
+        overlaeyEl.classList.toggle('active');
+        bodyEl.classList.toggle('noscroll');
+    });
+
+    // Клик по мобильному меню
+    mobMenu.addEventListener('click', function(){
+        this.classList.remove('active');
+        menuToggle.classList.remove('active');
+        overlaeyEl.classList.remove('active');
+        bodyEl.classList.remove('noscroll');
+    });
+
+    // Закрытие мобильного меню при клике по overlaey
+    overlaeyEl.addEventListener('click', function(){
+        this.classList.remove('active');
+        menuToggle.classList.remove('active');
+        mobMenu.classList.remove('active');
+        bodyEl.classList.remove('noscroll');
+    });
+
+    // Закрытие мобильного меню при ресайзе
+    window.addEventListener('resize', function(){
+        mobMenu.classList.remove('active');
+        menuToggle.classList.remove('active');
+        overlaeyEl.classList.remove('active');
+        bodyEl.classList.remove('noscroll');
+    });
+})
+
 // FORM PLACEHOLDER
 
 const formInputs = document.querySelectorAll('.form-field');
