90170225f17e4c3391a92ca857f2a4a1d6eee4e4
Vladimir <win_1994@mail.ru>
2020-09-14 18:15:55 +0300

    nav right

--- docs/js/main.js
+++ docs/js/main.js
@@ -64,6 +64,19 @@ $(document).ready(function(){
             block: ""
         }
     });
+
+    // Боковая навигация
+    $('#page-nav').onePageNav({
+        currentClass:'active',
+        changeHash: false,
+        scrollSpeed: 750,
+        scrollThreshold: 0.5,
+        filter:'',
+        easing:'swing',
+        begin: function () {},
+        end: function () {},
+        scrollChange: function ($currentListItem) {}
+    });
 });
 
 // FORM VALIDATE
