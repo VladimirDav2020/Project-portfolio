bae3ce3a628d115c98296a25a415d0808ab33be5
Vladimir <win_1994@mail.ru>
2020-09-18 14:21:36 +0300

    new works

---
+++ lessons/lesson_7_Unit/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+
+        nav: true,
+
+        navText: ['', ''],
+
+        loop: true,
+
+        smartSpeed: 700
+    });
+  });
